5da0750aa050ca096967184b95852d3c143f82a8
sinpoussi-cyber <sinpoussi@gmail.com>
2025-10-18 23:34:31 +0000

    Update useCompanies.ts

--- src/lib/hooks/useCompanies.ts
+++ src/lib/hooks/useCompanies.ts
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useCallback } from 'react';
-import { getCompanies, type CompanyDetail } from '../api/companiesApi';
+import { getCompanies, type CompanyDetail } from '../api/companies';
 
 export const useCompanies = () => {
   const [companies, setCompanies] = useState<CompanyDetail[]>([]);
