6fd67920fcb4e3b2cd641b0e135bf04a5235b1e6
sinpoussi-cyber <sinpoussi@gmail.com>
2025-10-21 21:52:35 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,6 +12,7 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://brvm-api-xode.onrender.com',
     NEXT_PUBLIC_API_VERSION: process.env.NEXT_PUBLIC_API_VERSION || '/api/v1',
+    NEXT_PUBLIC_API_USE_DIRECT: process.env.NEXT_PUBLIC_API_USE_DIRECT || 'false',
   },
   webpack(config) {
     const tsconfigPath = path.resolve(__dirname, 'tsconfig.json');
