f8a65f4ad925e910b2a8f91c8c97f5d73b906fe5
sinpoussi-cyber <sinpoussi@gmail.com>
2025-10-26 20:38:25 +0000

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,13 +1,18 @@
 // src/config/api.js
-export const API_BASE_URL = "https://brvm-api-xode.onrender.com/api/v1/market";
+
+const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL ||
+  "https://brvm-api-xode.onrender.com/api/v1/market";
 
 export async function fetchData(endpoint) {
   try {
-    const res = await fetch(`${API_BASE_URL}${endpoint}`, { cache: "no-store" });
-    if (!res.ok) throw new Error(`Erreur ${res.status}`);
-    return await res.json();
-  } catch (err) {
-    console.error("Erreur API:", err);
+    const response = await fetch(`${API_BASE_URL}${endpoint}`, {
+      cache: "no-store",
+    });
+    if (!response.ok) throw new Error(`Erreur ${response.status}`);
+    return await response.json();
+  } catch (error) {
+    console.error("Erreur API:", error);
     return null;
   }
 }
