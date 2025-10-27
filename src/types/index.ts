bffc547da79c0c7b1c8293870e689fa597910592
sinpoussi-cyber <sinpoussi@gmail.com>
2025-10-27 23:20:39 +0000

    Create index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,18 @@
+export type MarketOverview = {
+  overview: {
+    avg_change_percent: number;
+    total_volume: number;
+    total_companies: number;
+  };
+  top_sectors: { sector: string; total_volume: number }[];
+};
+
+export type TopMove = {
+  symbol: string;
+  latest_price: number;
+  change_percent: number;
+};
+
+export type TopMovesResponse = {
+  data: TopMove[];
+};
