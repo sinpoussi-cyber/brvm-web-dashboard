a8e315148f8eee2d84d30bd2395266c3436b544e
sinpoussi-cyber <sinpoussi@gmail.com>
2025-10-30 16:55:41 +0000

    Create page.tsx

---
+++ app/contact/page.tsx
@@ -0,0 +1,12 @@
+export default function Page(){
+  return (
+    <div className="space-y-2">
+      <h1 className="text-2xl font-bold">Nous contacter</h1>
+      <div className="rounded-xl border p-6">
+        <div>Email : contact@tondomaine.ci</div>
+        <div>Téléphone : +225 xx xx xx xx</div>
+        <div>Adresse : Abidjan, Côte d’Ivoire</div>
+      </div>
+    </div>
+  );
+}
