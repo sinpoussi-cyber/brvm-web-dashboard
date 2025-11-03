426ffe0fa2fda3e208998d364c83385de9e3de7d
sinpoussi-cyber <sinpoussi@gmail.com>
2025-11-03 17:24:16 +0000

    Update Card.tsx

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,10 +1,1 @@
-// src/components/ui/Card.tsx
-import React from 'react';
-
-export default function Card({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="bg-white shadow rounded-2xl p-4 border border-gray-100">
-      {children}
-    </div>
-  );
-}
+export { default } from '../../../components/ui/Card';
